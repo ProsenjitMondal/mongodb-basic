5c058e066ceacae5a74fac10d7a420c8937d8031
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2018-12-31 10:57:10 +0600

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,60 +1,33 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var app = express();
+app.use(bodyParser.json());
 
-var Todo = mongoose.model('Todo', {
-    text:{
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+app.post('/todos', (req, res) => {
+    // var todo = new Todo({
+    //     text: req.body.text
+    // });
+
+    // todo.save().then((doc) => {
+    //     res.send(doc);
+    // }, (e) => {
+    //     res.status(400).send(e);
+    // });
+
+    var otherTodo = new Todo({
+        text: req.body.text
+    });
+    otherTodo.save().then((docs) => {
+        res.send(docs);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
-
-// var newTodo = new Todo({
-//     text: 'Making a new todo'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved Todo ', doc);
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
-
-// var newTodo = new Todo({
-//     text: true
-// });
-// newTodo.save().then((doc) => {
-//     console.log('Saved Todo', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
-
-var User = mongoose.model('User', {
-    email:{
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-var newUser = new User({
-    email: 'prosenjit@gmail.com'
-});
-
-newUser.save().then((doc) => {
-    console.log('Saved User ', doc);
-}, (e) => {
-    console.log('Unable to save user', e)
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
 });
