14c03d1c37fba8cc61b0db895b487f64b04e7e66
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-01-06 13:42:13 +0600

    Try New mlab connection 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const dbURI = 'mongodb://Prosenjit:Pr@senjeet007@ds149744.mlab.com:49744/db_node_heroku';
+const dbURI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
 mongoose.connect(dbURI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {
