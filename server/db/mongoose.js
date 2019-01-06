887f010859fe62427188fe5a6dcc120ca44a7d1a
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-01-06 20:13:09 +0600

    Update mongodb database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const MONGOGB_URI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
-mongoose.connect(MONGOGB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+
+var port = process.env.PORT || 3000;
+if(port !== 3000) {
+    var MONGOGB_URI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
+}else{
+    var MONGOGB_URI = 'mongodb://localhost:27017/TodoApp';
+}
+
+mongoose.connect(MONGOGB_URI, {useNewUrlParser: true});
 
 module.exports = {
     mongoose
