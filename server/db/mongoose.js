fbecfc8ed525f01ceb23becd3116cf241314c17a
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-01-06 14:11:21 +0600

    MONGODB_URI database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+const MONGOGB_URI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
+mongoose.connect(MONGOGB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {
     mongoose
