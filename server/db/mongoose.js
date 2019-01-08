8d03d080171568ba14c5a6732bff1c254756ec9d
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-01-08 12:07:16 +0600

    Setup separate test, development and production envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,17 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var port = process.env.PORT || 3000;
-if(port !== 3000) {
-    var MONGOGB_URI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
-}else{
+var env = process.env.NODE_ENV || 'development';
+console.log('env ***** ', env);
+
+if(env === 'development') {
+    process.env.PORT = 3000;
     var MONGOGB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env === 'test') {
+    process.env.PORT = 3000;
+    var MONGOGB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}else if(env === 'production') {
+    var MONGOGB_URI = 'mongodb://prosenjit:prosenjit007@ds149744.mlab.com:49744/db_node_heroku';
 }
 
 mongoose.connect(MONGOGB_URI, {useNewUrlParser: true});
